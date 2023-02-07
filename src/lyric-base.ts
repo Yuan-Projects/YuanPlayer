f4173ed47147936548fae19c06840c577a8b1002
rainyjune <rainyjune@live.cn>
2023-02-07 11:10:44 +0800

    Remove yuanjs

--- src/lyric-base.ts
+++ src/lyric-base.ts
@@ -1,13 +1,10 @@
-// @ts-ignore
-import { ajax } from './utils';
-import Emitter from './emitter';
-import { LyricBaseOptions } from './lyric.d';
-
+import Emitter from "./emitter";
+import { LyricBaseOptions } from "./lyric.d";
 
 class LyricBase extends Emitter {
   lyricObj: {
-    timeArray: Array<any>,
-    lyricArray: Array<any>
+    timeArray: Array<any>;
+    lyricArray: Array<any>;
   } = {
     timeArray: [],
     lyricArray: []
@@ -25,23 +22,23 @@ class LyricBase extends Emitter {
   parseLyricItems(items: any) {
     var result: Array<any> = [];
     var timePattern = /\[[0-9]{2}:[0-9]{2}\.[0-9]{2,3}\]/g;
-    for(var i = 0, l = items.length; i < l; i++) {
+    for (var i = 0, l = items.length; i < l; i++) {
       var thisItem = items[i];
       var timeSpanArray = thisItem.match(timePattern);
       if (timeSpanArray) {
         var lyric = thisItem.split(timePattern).pop();
         for (var j = 0, len = timeSpanArray.length; j < len; j++) {
-          result.push(timeSpanArray[j]+lyric);
+          result.push(timeSpanArray[j] + lyric);
         }
-      };
+      }
     }
     return result;
   }
-  logLyricInfo(items: any){
+  logLyricInfo(items: any) {
     var patt = /\[|\]/;
     for (var i = 0; i < items.length; i++) {
       var component = items[i].split(patt);
-      if (component[2] === '') {
+      if (component[2] === "") {
         // If no lyric
       }
       this.lyricObj.timeArray.push(this.parseTimeToSeconds(component[1]));
@@ -49,52 +46,55 @@ class LyricBase extends Emitter {
     }
   }
 
-  compareTimeSpan(x: any,y:any): number {
+  compareTimeSpan(x: any, y: any): number {
     var timePattern = /\[([0-9]{2}:[0-9]{2}\.[0-9]{2,3})\]/;
-    var xTime = x.match(timePattern)[1], yTime = y.match(timePattern)[1];
-    var xTimeInSeconds: number = this.parseTimeToSeconds(xTime), yTimeInSeconds: number = this.parseTimeToSeconds(yTime);
+    var xTime = x.match(timePattern)[1],
+      yTime = y.match(timePattern)[1];
+    var xTimeInSeconds: number = this.parseTimeToSeconds(xTime),
+      yTimeInSeconds: number = this.parseTimeToSeconds(yTime);
     return xTimeInSeconds - yTimeInSeconds;
   }
 
   parseTimeToSeconds(timeString: string): number {
-    var component = timeString.split('.');
+    var component = timeString.split(".");
     var bigPart = component[0];
-    var bigPartComponent = bigPart.split(':');
+    var bigPartComponent = bigPart.split(":");
     var minutePart = parseInt(bigPartComponent[0]);
     var secondPart = parseInt(bigPartComponent[1]);
-    return parseFloat(minutePart * 60 + secondPart + '.' + component[1]);
+    return parseFloat(minutePart * 60 + secondPart + "." + component[1]);
   }
 
   addLyric() {
-    var that = this;
     var lyric = this.lyric;
     if (lyric) {
-      if (typeof lyric === 'string') {  
-        if (lyric.substr(0, 8) === 'https://' || lyric.substr(0, 7) === 'http://') {
-          ajax({url:lyric, contentType: "text/plain"}).then(function(lyricText: any){
-            var lyricItems = lyricText.responseText.split(/[\n\r]/g);
-            lyricItems = that.parseLyricItems(lyricItems);
-            lyricItems.sort(function(x: any,y: any){ return that.compareTimeSpan.call(that,x,y);});
-            // TODO
-            that.trigger('lyricFetched', lyricItems);
-            that.logLyricInfo(lyricItems);
-          },function(err: any){
-            console.log('error:', err);
-          });
-        }
-      }
+      var lyricItems = lyric.split(/[\n\r]/g);
+      lyricItems = this.parseLyricItems(lyricItems);
+      lyricItems.sort((x: any, y: any) => {
+        return this.compareTimeSpan.call(this, x, y);
+      });
+      // TODO
+      this.trigger("lyricFetched", lyricItems);
+      this.logLyricInfo(lyricItems);
     }
   }
 
   bindLyricEvents() {
     var that = this;
     var media = this.mediaObject;
-    if (!media) return ;
-    media.addEventListener('timeupdate', function(){
-      if (that.lyric && that.lyricObj.timeArray.length && that.lyricObj.lyricArray.length) {
-        that.trigger('timeupdated', media.currentTime);
-      }
-    }, false);
+    if (!media) return;
+    media.addEventListener(
+      "timeupdate",
+      function () {
+        if (
+          that.lyric &&
+          that.lyricObj.timeArray.length &&
+          that.lyricObj.lyricArray.length
+        ) {
+          that.trigger("timeupdated", media.currentTime);
+        }
+      },
+      false
+    );
   }
 
   loadLyricPlugin() {
