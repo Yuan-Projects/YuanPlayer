6f6dc7a00474921c2c3974fb80643883b6c1c567
rainyjune <rainyjune@live.cn>
2023-02-15 21:00:08 +0800

    Update the use() function

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,38 +2,17 @@ import Player from "./player";
 import Lyric from "./lyric";
 import PlayList from "./playlist";
 
-const YuanPlayer: any = {
-  themes: {},
-  Player: function (options) {
-    if (options.theme && YuanPlayer.themes[options.theme]) {
-      return new YuanPlayer.themes[options.theme].Player(options);
-    }
-  },
-  Lyric: function (options) {
-    if (options.theme && YuanPlayer.themes[options.theme]) {
-      return new YuanPlayer.themes[options.theme].Lyric(options);
-    }
-  },
-  PlayList: function (options) {
-    if (options.theme && YuanPlayer.themes[options.theme]) {
-      return new YuanPlayer.themes[options.theme].PlayList(options);
-    }
+const YuanPlayer = {
+  Player,
+  Lyric,
+  PlayList,
+  use: function (themeObject) {
+    return {
+      Player: themeObject.Player ? themeObject.Player(Player) : null,
+      Lyric: themeObject.Lyric ? themeObject.Lyric(Lyric) : null,
+      PlayList: themeObject.PlayList ? themeObject.PlayList(PlayList) : null
+    };
   }
 };
-
-YuanPlayer.use = function (themeObject) {
-  if (!YuanPlayer.themes[themeObject.name]) {
-    YuanPlayer.themes[themeObject.name] = {};
-  }
-  if (themeObject.Player) {
-    YuanPlayer.themes[themeObject.name].Player = themeObject.Player(Player);
-  }
-  if (themeObject.Lyric) {
-    YuanPlayer.themes[themeObject.name].Lyric = themeObject.Lyric(Lyric);
-  }
-  if (themeObject.PlayList) {
-    YuanPlayer.themes[themeObject.name].PlayList = themeObject.PlayList(PlayList);
-  }
-}
 
 export default YuanPlayer;
