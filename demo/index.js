ac3e698a65f27f2b3d26b9aa64ad5ed9094d068f
rainyjune <rainyjune@live.cn>
2023-03-02 14:21:38 +0800

    Update demo files

--- demo/index.js
+++ demo/index.js
@@ -1,73 +1,51 @@
-const YPlayerObj = YuanPlayer.use(YuanPlayerThemeYuan);
-const BMPlayerObj = YuanPlayer.use(YuanPlayerThemeBlueMonday);
-const PFPlayerObj = YuanPlayer.use(YuanPlayerThemePinkFlag);
-
-const { Player, Lyric, PlayList } = YuanPlayer;
-const YPlayer = YPlayerObj.Player, YLyric = YPlayerObj.Lyric, YPlayList = YPlayerObj.PlayList;
-const BMPlayer = BMPlayerObj.Player, BMLyric = BMPlayerObj.Lyric, BMPlayList = BMPlayerObj.PlayList;
-const PFPlayer = PFPlayerObj.Player, PFPlayList = PFPlayerObj.PlayList;
-
-fetchLyrics(playlist.map(item => item.lyric)).then(lyricArr => {
-  lyricArr.forEach((lyric, index) => {
-    playlist[index].lyric = lyric;
-  })
-}).then(function() { // Theme 1: Yuan
-  var player3 = new YPlayer({
-    media: {src: playlist[0].src},
-    container: document.querySelector('#playercontainer3')
-  });
-  var lyric3 = new YLyric({
-    lyric: playlist[0].lyric,
-    mediaObject: player3.mediaObject,
-    container: document.getElementById('lyriccontainer3')
-  });
-  lyric3.loadLyricPlugin();
-  window.list3 = new YPlayList({
-    container: document.getElementById('listcontainer3'),
-    list: playlist,
-    player: player3,
-    lyricObj: lyric3
-  });
-}).then(() => { // Theme 2: blueMonday
-  var blueMondayPlayer1 = new BMPlayer({
-    media: {src: playlist[0].src},
+var BMPlayer = YuanPlayer.use(YuanPlayerThemeBlueMonday);
+var PFPlayer = YuanPlayer.use(YuanPlayerThemePinkFlag);
+/*
+window.noThemePlayer = new YuanPlayer({
+  media: playlist[0],
+  nativeControls: true,
+  container: document.querySelector('#noThemeContainer')
+});
+*/
+  window.bmplayer = new BMPlayer({
+    media: playlist,
     container: document.querySelector('#blueMondayPlayerContainer1')
   });
-  var blueMondayLyric1 = new BMLyric({
-    lyric: playlist[0].lyric,
-    mediaObject: blueMondayPlayer1.mediaObject,
-    container: document.getElementById('blueMondayPlayerContainer1')
+
+  setTimeout(function() {
+    //bmplayer.setMedia(playlist[0])
+    //debugger;
+    //bmplayer.setPlaylist(playlist);
+  }, 1000)
+  let count = 0;
+/*
+  bmplayer.on('pause', () => {
+    debugger;
   });
-  blueMondayLyric1.loadLyricPlugin();
-  window.blueMondayPlayListInstance = new BMPlayList({
-    autoPlay: false,
-    container: document.querySelector('#blueMondayPlayerContainer1'),
-    list: playlist,
-    player: blueMondayPlayer1,
-    lyricObj: blueMondayLyric1
+
+  const playFn = () => {
+    count++;
+    debugger;
+    if (count > 2) {
+      bmplayer.off('play', playFn);
+    }
+  };
+  bmplayer.on('play', playFn);
+  bmplayer.on('add', (arg) => {
+    debugger
   });
-}).then(() => { // Theme 3: pinkFlag
-  let pfPlayer1 = new PFPlayer({
-    media: {title: playlist[0].title, src: playlist[0].src},
+*/
+  
+  // Theme 3: pinkFlag
+  window.pfPlayer1 = new PFPlayer({
+    loop: 'all',
+    media: {},
     container: document.querySelector('#pinkFlagPlayerContainer1')
   });
-  //debugger;
-  window.pinFlagPlayListInstance = new PFPlayList({
-    autoPlay: false,
-    container: document.querySelector('#pinkFlagPlayerContainer1'),
-    list: playlist,
-    player: pfPlayer1,
-  });
-})
 
-async function fetchLyrics(urls) {
-  try {
-    const fetchArr = urls.map(url => {
-      return fetch(url).then(res => res.text())
-    });
-    const lyrics = await Promise.all(fetchArr);
-    return lyrics;
-  } catch (err) {
-    console.log(err);
-  }
-}
+  setTimeout(function() {
+    //debugger;
+    pfPlayer1.setPlaylist(playlist);
+    //pfPlayer1.setMedia(playlist[0])
+  })
+
--- demo/playlist.js
+++ demo/playlist.js
@@ -1,11 +1,11 @@
-var playlist = [
+window.playlist = [
   {
     title: 'Let it go',
     artist: 'Idina Menzel',
     poster: 'https://y.qq.com/music/photo_new/T002R300x300M000000nmCPL1H8bES_1.jpg?max_age=2592000',
     src: 'media/letitgo.m4a',
     loop: false,
-    lyric: 'lyrics/letitgo.lrc'
+    lyric: "[00:00.51]Let It Go - Idina Menzel"+"\n    [00:14.08]The snow glows white on the mountain tonight"+"\n    [00:17.19]Not a footprint to be seen"+"\n    [00:20.86]A kingdom of isolation,"+"\n    [00:22.87]"+"\n    [00:24.30]and it looks like I'm the Queen"+"\n    [00:28.55]The wind is howling like this swirling storm inside"+"\n    [00:35.58]Couldn't keep it in; Heaven knows I've tried"+"\n    [00:42.59]Don't let them in, don't let them see"+"\n    [00:45.91]Be the good girl you always have to be"+"\n    [00:49.40]Conceal, don't feel, don't let them know"+"\n    [00:55.76]Well now they know"+"\n    [00:59.14]Let it go, let it go"+"\n    [01:02.61]Can't hold it back anymore"+"\n    [01:06.17]Let it go, let it go"+"\n    [01:09.39]Turn away and slam the door"+"\n    [01:13.18]I don't care what they're going to say"+"\n    [01:19.82]Let the storm rage on"+"\n    [01:24.21]The cold never bothered me anyway"+"\n    [01:28.38]"+"\n    [01:31.69]It's funny how some distance"+"\n    [01:34.31]Makes everything seem small"+"\n    [01:37.75]And the fears that once controlled me"+"\n    [01:41.18]Can't get to me at all"+"\n    [01:45.66]It's time to see what I can do"+"\n    [01:49.20]To test the limits and break through"+"\n    [01:52.80]No right, no wrong, no rules for me,"+"\n    [01:56.94]I'm free!"+"\n    [02:00.40]Let it go, let it go"+"\n    [02:03.87]I am one with the wind and sky"+"\n    [02:07.21]Let it go, let it go"+"\n    [02:10.75]You'll never see me cry"+"\n    [02:14.49]Here I stand"+"\n    [02:18.22]And here I'll stay"+"\n    [02:21.62]Let the storm rage on"+"\n    [02:33.36]My power flurries through the air into the ground"+"\n    [02:39.79]My soul is spiraling in frozen fractals all around"+"\n    [02:46.80]And one thought crystallizes like an icy blast"+"\n    [02:53.73]I'm never going back, the past is in the past"+"\n    [03:01.78]Let it go, let it go"+"\n    [03:05.31]And I ll rise like the break of dawn"+"\n    [03:08.60]Let it go, let it go"+"\n    [03:12.19]That perfect girl is gone"+"\n    [03:16.25]Here I stand"+"\n    [03:18.87]In the light of day"+"\n    [03:24.85]Let the storm rage on"+"\n    [03:30.09]The cold never bothered me anyway!"+"\n    [03:35.09]"
   },
   {
     title: '再见中国海',
@@ -13,7 +13,7 @@ var playlist = [
     poster: 'http://p2.music.126.net/cbtvK52JoaIK0iZNJ2BtHQ==/109951165958877997.jpg?param=130y130',
     src: 'media/zaijianzhongguohai.m4a',
     loop: false,
-    lyric: 'lyrics/zaijianzhongguohai.lrc'
+    lyric: "[00:00.000] 作曲 : 林国荣"+"\n[00:01.000] 作词 : 黄桂兰"+"\n[00:24.300]望着蓝天 望着大海"+"\n[00:27.130]望着地平线"+"\n[00:29.900]想着爱 想着下一刻的思念"+"\n[00:34.989]当你温柔的指尖 滑过我沉默的脸"+"\n[00:41.190]别再哄我 怕泪水被你发现"+"\n[00:47.099]时间有限 空间有限"+"\n[00:50.269]更叫人依恋"+"\n[00:52.699]有些爱 缠绵因为没有终点"+"\n[00:58.289]我们一起许个愿"+"\n[01:00.829]不管有没有实现 时空再远"+"\n[01:05.689]想起来就在面前"+"\n[01:09.899]中国海GoodBye"+"\n[01:12.679]亲吻的脸别移开"+"\n[01:15.690]多想埋进你那深情的胸怀"+"\n[01:21.449]海角天涯 手心握着你的爱"+"\n[01:27.499]在心底喊着有天我要回来"+"\n[01:32.908]中国海GoodBye"+"\n[01:35.878]You are always on my mind"+"\n[01:38.709]记得你的笑和浪花一样白"+"\n[01:45.080]潮来潮往 是我思念的节拍"+"\n[01:50.719]当初的约定谁也不能更改"+"\n[01:57.829]"+"\n[02:17.060]望着蓝天 望着大海"+"\n[02:19.840]望着地平线"+"\n[02:22.090]想着爱 想着下一刻的思念"+"\n[02:27.600]当你温柔的指尖 滑过我沉默的脸"+"\n[02:33.800]别再哄我 怕泪水被你发现"+"\n[02:39.700]时间有限 空间有限"+"\n[02:42.820]更叫人依恋"+"\n[02:46.200]有些爱 缠绵因为没有终点"+"\n[02:51.090]我们一起许个愿"+"\n[02:53.879]不管有没有实现 时空再远"+"\n[02:58.400]想起来就在面前"+"\n[03:02.700]中国海GoodBye"+"\n[03:05.470]亲吻的脸别移开"+"\n[03:08.400]多想埋进你那深情的胸怀"+"\n[03:14.300]海角天涯 手心握着你的爱"+"\n[03:20.270]在心底喊着有天我要回来"+"\n[03:25.820]中国海GoodBye"+"\n[03:28.660]You are always on my mind"+"\n[03:31.410]记得你的笑和浪花一样白"+"\n[03:37.200]潮来潮往 是我思念的节拍"+"\n[03:43.460]当初的约定谁也不能更改"+"\n[03:49.400]中国海GoodBye"+"\n[03:52.100]You are always on my mind"+"\n[03:55.460]记得你的笑和浪花一样白"+"\n[04:00.700]"
   },
   {
     title: '守候',
@@ -21,7 +21,7 @@ var playlist = [
     poster: 'http://p2.music.126.net/Ijvh1PFFZz_ydSUTTeWfkw==/6627856093408607.jpg?param=130y130',
     src: 'media/shouhou.m4a',
     loop: false,
-    lyric: 'lyrics/shouhou.lrc'
+    lyric: "[00:00.00]守候-徐蓂蕾"+"\n[00:14.27]我能等你吗"+"\n[00:17.08]在那淡淡月光下"+"\n[00:19.80]静静想你"+"\n[00:25.17]我能等你吗"+"\n[00:27.99]在那熟悉的地方"+"\n[00:30.65]轻唤着你"+"\n[00:35.58]风里传来你的呼吸"+"\n[00:40.86]云里映着你的笑意"+"\n[00:46.29]林里的鸟相偎相依"+"\n[00:51.95]我却孤寂"+"\n[00:58.09]我等你回来"+"\n[01:00.80]把那窗儿打开"+"\n[01:03.42]向我依赖"+"\n[01:08.99]我等你回来"+"\n[01:11.61]带着纯真的风采"+"\n[01:14.37]宛如小孩"+"\n[01:19.35]衣上装满你的记忆"+"\n[01:24.53]夜里的梦多么清晰"+"\n[01:29.91]冰冷黎明只剩叹息"+"\n[01:35.49]如何忘你"+"\n[01:41.03]迷离的夜"+"\n[01:43.65]飘响着无边境的旋律"+"\n[01:49.14]在耳边旋绕不停"+"\n[01:53.21]能不能"+"\n[01:55.39]载着思绪的雨"+"\n[01:58.24]带我找你"+"\n[02:01.39]纵然是梦想也罢"+"\n[02:05.58]宁愿寂寞 放弃自由"+"\n[02:11.21]怎样也想抓住你的手"+"\n[02:16.93]春夏秋冬 你的承诺"+"\n[02:25.72]我会守候"+"\n[02:54.17]迷离的夜"+"\n[02:56.56]飘响着无边境的旋律"+"\n[03:02.03]在耳边旋绕不停"+"\n[03:06.25]能不能 载着思绪的雨"+"\n[03:11.33]带我找你"+"\n[03:14.22]纵然是梦想也罢"+"\n[03:18.43]宁愿寂寞 放弃自由"+"\n[03:24.06]怎样也想抓住你的手"+"\n[03:29.90]春夏秋冬 你的承诺"+"\n[03:39.66]我会守候"
   },
   {
     title: '給未來的自己',
@@ -29,6 +29,6 @@ var playlist = [
     poster: 'https://y.qq.com/music/photo_new/T002R300x300M000002oy2Mp3I8Rgo_1.jpg?max_age=2592000',
     src: 'media/1007000868010800.mp3',
     loop: false,
-    lyric: 'lyrics/to-the-future-myself.lrc'
+    lyric: "[00:01.00]梁靜茹-給未來的自己"+"\n[00:03.00]給未來的自己"+"\n[00:03.89]梁靜茹"+"\n[00:07.54]作詞：黃婷 作曲：李正宗"+"\n[00:14.00]站在狂風的天臺一望無際"+"\n[00:19.65]這一座孤獨的城市"+"\n[00:24.90]在天空與高樓交接的盡頭"+"\n[00:33.03]誰追尋空曠的自由"+"\n[00:39.00]陽光覆滿這一刻寧靜的我"+"\n[00:45.37]隔絕了喧囂和冷漠"+"\n[00:52.00]川流不息的人遊蕩在街頭"+"\n[00:58.65]誰能聽見誰的寂寞"+"\n[01:03.92]找一個人心心相惜　找一顆心心心相印"+"\n[01:12.03]在這個宇宙　我是獨一無二　沒人能取代"+"\n[01:16.75][02:44.30][03:36.50]不管怎樣　怎樣都會受傷"+"\n[01:21.00][02:48.17][03:40.75]傷了又怎樣　至少我很堅強　我很坦蕩"+"\n[01:41.37]夜幕籠罩燦爛的一片燈海"+"\n[01:47.09]多少人多少種無奈"+"\n[01:53.40]在星光裏遺忘昨天的傷害"+"\n[02:00.43]一覺醒來還有期待"+"\n[02:05.30][03:23.68]我不放棄愛的勇氣　我不懷疑會有真心"+"\n[02:13.00][03:30.50]我要握住　一個最美的夢　給未來的自己"+"\n[02:18.53]一天一天　一天推翻一天　堅持的信仰"+"\n[02:25.40]我會記住自己今天的模樣"+"\n[02:31.26]有一個人心心相惜　有一顆心心心相印"+"\n[02:38.00]拋開過去　我想認真去追尋　未來的自己"+"\n[03:48.96]未來的你　會懂我的瘋狂"+"\n[01:31.50][02:58.50]"
   }
 ];
