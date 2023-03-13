3aa95924e02c0a3070d6103567e187a5c27f79fd
rainyjune <rainyjune@live.cn>
2023-03-13 20:21:34 +0800

    Add demo/analytics.js

---
+++ demo/analytics.js
@@ -0,0 +1,13 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-MMCQPP0JGR');
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?14d0ebd37f0984feeca761a240d4b495";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
