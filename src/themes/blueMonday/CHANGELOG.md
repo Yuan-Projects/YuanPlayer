45370df9445f7b92430bf7dab3be07ce05b5958b
rainyjune <rainyjune@live.cn>
2023-03-21 10:22:03 +0800

    feat: Add text tracks support for yuanplayer-theme-bluemonday

--- src/themes/blueMonday/CHANGELOG.md
+++ src/themes/blueMonday/CHANGELOG.md
@@ -1,3 +1,8 @@
+## 1.2.0 (March 21, 2023)
+
+- Add text tracks support
+- Optimise CSS rules to reduce footprint
+
 ## 1.1.1 (March 13, 2023)
 
 - Optimise user experience by adding a loading indicator when media file is loading
--- src/themes/blueMonday/player.ts
+++ src/themes/blueMonday/player.ts
@@ -38,6 +38,41 @@ function getClass(Base) {
       if (document.createElement("p").style.flexWrap === '') {
         div.querySelector('.yuan-interface')?.classList.add('flexbox');
       }
+
+      const cclist = div.querySelector('.cclist') as HTMLDivElement;
+      const ccbtn = div.querySelector('.yuan-closed-caption') as HTMLElement;
+      cclist.addEventListener('click', (e) => {
+        const target = e.target as HTMLElement;
+        if (target.tagName !== 'INPUT') return false;
+        const value = (target as HTMLInputElement).value;
+        this.setActiveCC(value);
+        cclist.innerHTML = '';
+        this.updateCCButton();
+      });
+
+      ccbtn.addEventListener('click', () => {
+        this.showList();
+      });
+    }
+    showList() {
+      var list = this.getCCList();
+      var ul = document.createElement('ul');
+      list.forEach(function(item) {
+        var li = document.createElement('li');
+        var label = document.createElement('label');
+        label.textContent = item.label;
+        var radio = document.createElement('input');
+        radio.type = 'radio';
+        radio.name = 'cc';
+        radio.value = item.label;
+        radio.checked = item.checked;
+        label.appendChild(radio);
+        li.appendChild(label);
+        ul.appendChild(li);
+      });
+      const cclist = this.container.querySelector('.cclist') as HTMLDivElement;
+      cclist.innerHTML = "";
+      cclist.appendChild(ul);
     }
   }
 }
