43c69e1218b5149d4a632d6d616f73616751429b
rainyjune <rainyjune@live.cn>
2023-04-05 12:17:57 +0800

    chore: Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     //baseURL: process.env.CI ? '' : 'http://127.0.0.1:3000',
-    baseURL: process.env.CI ? 'https://yuan-projects.github.io/YuanPlayer/' : 'http://localhost/YuanPlayer/',
+    //baseURL: process.env.CI ? 'https://yuan-projects.github.io/YuanPlayer/' : 'http://localhost/YuanPlayer/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
