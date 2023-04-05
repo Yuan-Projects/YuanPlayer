8159a125d48ea0a64dcf793b9fc2e2e2d8b85e3c
rainyjune <rainyjune@live.cn>
2023-04-05 15:06:07 +0800

    test: Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,8 +25,10 @@ const config = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://127.0.0.1:3000',
     //baseURL: process.env.CI ? '' : 'http://127.0.0.1:3000',
-    baseURL: process.env.CI ? 'https://yuan-projects.github.io/YuanPlayer/' : 'http://127.0.0.1:3000',
+    //baseURL: process.env.CI ? '' : 'http://127.0.0.1:3000',
+    //baseURL: process.env.CI ? 'https://yuan-projects.github.io/YuanPlayer/' : 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
